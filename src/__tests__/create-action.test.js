3596b03a10265821ba2136cf179bac428391d9a0
Vedran Blazenka <blazenka.vedran@gmail.com>
2018-02-19 15:53:23 +0100

    Finished createAction.

--- src/__tests__/create-action.test.js
+++ src/__tests__/create-action.test.js
@@ -1,43 +1,40 @@
 import test from 'ava';
-import createAction from '../create-action';
-import createRequestTypes from '../create-request-types';
-import createActionsFromTypes from '../create-actions-from-types';
+
+import {isFSA} from 'flux-standard-action';
+import {createAction} from '../../dist';
 
 // Constants
 const SIMPLE_ACTION_CONSTANT = 'SIMPLE_ACTION_CONSTANT';
 
 let simpleAction;
-let SIMPLE_NETWORK_CONSTANT_RT;
-let simpleActionConstantRT;
 
 // Prepare tests
 test.before(() => {
   // Create action creator without payload
   simpleAction = createAction(SIMPLE_ACTION_CONSTANT);
+});
 
-  // Create network constants with suffixes
-  SIMPLE_NETWORK_CONSTANT_RT = createRequestTypes('SIMPLE_NETWORK_CONSTANT_RT');
-
-  // Create action creators from network constants
-  simpleActionConstantRT = createActionsFromTypes(SIMPLE_NETWORK_CONSTANT_RT);
+test('createActions returns valid flux standard action', t => {
+  t.is(isFSA(simpleAction()), true);
 });
 
 test('createAction should return action creator with a type', t => {
   t.deepEqual(simpleAction(), {
-    type: SIMPLE_ACTION_CONSTANT
+    type: SIMPLE_ACTION_CONSTANT,
+    error: false
   });
 });
 
-test('createRequestTypes should return a list of types with request suffixes', t => {
-  t.deepEqual(SIMPLE_NETWORK_CONSTANT_RT, {
-    REQUESTED: SIMPLE_NETWORK_CONSTANT_RT.REQUESTED,
-    COMPLETED: SIMPLE_NETWORK_CONSTANT_RT.COMPLETED,
-    FAILED: SIMPLE_NETWORK_CONSTANT_RT.FAILED
-  });
-});
+test('createAction should return action with custom payload', t => {
+  const customPayloadAction = createAction(SIMPLE_ACTION_CONSTANT, payload => ({
+    newProp: payload
+  }));
 
-test('createActionsFromTypes should return a list of action creators with request types', t => {
-  t.deepEqual(simpleActionConstantRT.requested(), {
-    type: SIMPLE_NETWORK_CONSTANT_RT.REQUESTED
+  t.deepEqual(customPayloadAction('lorem ispum'), {
+    type: SIMPLE_ACTION_CONSTANT,
+    payload: {
+      newProp: 'lorem ispum'
+    },
+    error: false
   });
 });
--- src/create-action.js
+++ src/create-action.js
@@ -1,12 +1,29 @@
-function action(type, payload) {
-  return (
-    (typeof payload !== 'undefined') &&
-      payload.constructor.name !== 'SyntheticMouseEvent'
-  ) ? {type, payload} : {type};
+import {identity, isFunction, isUndefined} from 'lodash';
+
+export default function (type, payloadCreator = identity, metaCreator) {
+  const hasPayload = !isUndefined(payloadCreator);
+  const finalPayloadCreator = isFunction(payloadCreator) ? payloadCreator : identity;
+  const hasMetaCreator = isFunction(metaCreator);
+
+  const createAction = (...args) => {
+    const payload = hasPayload ? finalPayloadCreator(...args) : payloadCreator;
+
+    const action = {
+      type,
+      error: payload instanceof Error
+    };
+
+    if (payload) {
+      action.payload = payload;
+    }
+
+    if (hasMetaCreator) {
+      action.meta = metaCreator(...args);
+    }
+
+    return action;
+  };
+
+  return createAction;
 }
 
-function createAction(type) {
-  return payload => action(type, payload);
-}
-
-module.exports = createAction;
